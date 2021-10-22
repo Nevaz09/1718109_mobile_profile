dfd450f527146264515bab714409cbac114b8e35
Nevaz09 <83652280+Nevaz09@users.noreply.github.com>
2021-10-22 12:01:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a1718109_mobile_profile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a1718109_mobile_profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
